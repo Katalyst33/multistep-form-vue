6dda9d3d517b35381dc55a54a74350196b90155d
hibrid <samuelnmeje@gmail.com>
2020-04-16 01:18:57 +0400

    pass mama form data  to child form 🙃

--- src/components/FormReviewOrder.vue
+++ src/components/FormReviewOrder.vue
@@ -65,9 +65,12 @@
         </div>
 
         <div class="w-1/3">
-          <h3>RECIPIENT</h3>
+          <h3>
+            {{wizardData.recipient}}
+          </h3>
           <p class="leading-normal">
-            ADDRESS
+            {{wizardData.address}}
+
           </p>
         </div>
       </div>
@@ -77,6 +80,12 @@
 
 <script>
   export default {
+    props:{
+    wizardData:{
+      type:Object,
+      required:true
+    },
+    },
     data () {
       return {
         form: {
@@ -87,7 +96,14 @@
     },
     computed: {
       totalPrice () {
-        return 0
+        let total = this.wizardData.plan.price
+        if(this.form.chocolate){
+          total += 4
+        }
+        if(this.form.otherTreat){
+          total += 2
+        }
+        return total
       }
     },
 
